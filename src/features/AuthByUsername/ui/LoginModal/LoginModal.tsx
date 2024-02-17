959a55f470159cd0d68a66f4576143a81d6ee01e
pepega4real <kiseleff00@gmail.com>
2024-02-17 23:49:00 +0300

    add pre-comit

--- src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
+++ src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
@@ -1,7 +1,6 @@
 import { classNames } from 'shared/lib/classNames/classNames'
 import { Modal } from 'shared/ui/Modal/Modal'
 import { LoginForm } from '../LoginForm/LoginForm'
-import cls from './LoginModal.module.scss'
 
 interface LoginModalProps {
   className?: string
@@ -11,12 +10,7 @@ interface LoginModalProps {
 
 export const LoginModal = ({ className, isOpen, onClose }: LoginModalProps) => {
   return (
-    <Modal
-      isOpen={isOpen}
-      onClose={onClose}
-      className={classNames(cls.LoginModal, {}, [className])}
-      lazy
-    >
+    <Modal isOpen={isOpen} onClose={onClose} className={classNames('', {}, [className])} lazy>
       <LoginForm />
     </Modal>
   )
